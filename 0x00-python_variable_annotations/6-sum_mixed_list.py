fe0c29dc4c6c2fac28ce9480e00541a788167d15
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-08-01 23:27:00 +0300

    add Documentation please

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,6 +7,10 @@ returns their sum as a float.
 
 
 def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """
+    takes a list mxd_lst of integers and floats and returns
+    their sum as a float.
+    """
     x = 0.0
     for val in mxd_lst:
         x += val
