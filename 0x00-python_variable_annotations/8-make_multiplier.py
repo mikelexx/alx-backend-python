92902b0e74bc5cc4f816d5581af64aeb2ec9b4f7
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-08-02 00:42:37 +0300

    multiply argument by multiplier clouser argument

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 """
-Write a type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier.
+Write a type-annotated function make_multiplier that takes a
+float multiplier as argument and
+returns a function that multiplies a float by multiplier.
 """
 import typing
 
-def multiply(multiplier: float)->float:
-    return multiplier **2
 
-def make_multiplier(multiplier: float)->typing.Callable[[float], float]:
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """
      takes a float multiplier as argument and returns a
      function that multiplies a float by multiplier.
     """
+
+    def multiply(value: float) -> float:
+        """mutliplies a value by mutliplier
+        supplied in the closure"""
+        return multiplier * value
+
     return multiply
