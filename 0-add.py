43377ffcef4a371b4efa8db79ff3373ffa10f70e
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-08-01 22:38:09 +0300

    0. Basic annotations-add

---
+++ 0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+contains type-annotated function add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    takes a float a and a float b as arguments
+    and returns their sum as a float.
+    """
+    return a + b
