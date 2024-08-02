ac75f4c9a577578aadd3f7cd3b0b8dbc194b559e
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-08-02 11:02:18 +0300

    12. Type Checking- using mypy :)

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate piece of code and apply any necessary changes.
+"""
+from typing import Tuple, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> list[Any]:
+    zoomed_in: list = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
