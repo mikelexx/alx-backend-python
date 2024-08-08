cb4b95b31828f362aeed4f8e1c298a8e85bc4764
mikelexx <michael.muri2020@gmail.com>
2024-08-08 05:42:40 +0300

    add function documentation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,6 +11,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+    measure_runtime of parallel async execution coroutines
+    """
     start = time.perf_counter()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
     stop = time.perf_counter()
